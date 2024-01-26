4fd89591799d3c26b07ee65807bd0e8898afdfdf
Yago Wagner <117683041+YagoWagner@users.noreply.github.com>
2024-01-26 15:55:08 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,17 @@
 var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador ++
+} while(contador <= 10)
+
+
+/*
+var contador = 1
 while(contador <= 6){
     console.log(`Passo ${contador}`)
     contador ++
 }
-
-
-
+*/ 
 
 
 /*
