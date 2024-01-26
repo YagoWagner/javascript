9542f7948c4275004e1537255e59dd5cdcff6781
Yago Wagner <117683041+YagoWagner@users.noreply.github.com>
2024-01-26 15:28:35 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador ++
+}
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
