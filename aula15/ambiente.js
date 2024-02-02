b148ab0a35f9c3e7d9e91b2a8d9bf54f27a49752
Yago Wagner <117683041+YagoWagner@users.noreply.github.com>
2024-02-02 16:42:13 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,8 +1,17 @@
 var num = [5, 8, 2, 9, 3]
 
-num.sort()
 num.push(1)
+num.sort()
+
 console.log(num)
 console.log(`Nosso vetor é o ${num}`)
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+
+if(pos == -1){
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
