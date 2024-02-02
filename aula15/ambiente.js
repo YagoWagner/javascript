58ed7db444f716651c98c5da97cb7f4364ee3b30
Yago Wagner <117683041+YagoWagner@users.noreply.github.com>
2024-02-02 16:22:35 -0300

    Update aula15
    vetor/array

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+var num = [5, 8, 2, 9, 3]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
